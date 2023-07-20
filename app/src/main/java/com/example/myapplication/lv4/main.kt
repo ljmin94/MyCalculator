b0a1ed65ddb796d75f36dd79d9324df7045795f4
ljmin94 <ljmin94@naver.com>
2023-07-20 20:13:24 +0900

    git test

--- app/src/main/java/com/example/myapplication/lv4/main.kt
+++ app/src/main/java/com/example/myapplication/lv4/main.kt
@@ -7,5 +7,6 @@ fun main() {
     println("뺄셈 연산 결과: ${calculate.calculator(Subtract(), 20, 2)}")
     println("곱셈 연산 결과: ${calculate.calculator(Multiply(), 10, 10)}")
     println("나눗셈(몫) 연산 결과: ${calculate.calculator(Divide(), 16, 4)}")
+    println("계산 끝!!!!!!!!!!")
 
 }
